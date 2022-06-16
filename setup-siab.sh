1a51fb9b9c52f4f813c5ee2819551752160d31e1
Chad J. Hamilton <chamilton@github.com>
2022-06-16 16:09:14 -0400

    Updated setup-siab.sh for faster rebuilds

--- setup-siab.sh
+++ setup-siab.sh
@@ -16,7 +16,27 @@ else
     oc start-build siab --from-dir . --follow -n ${PROJECT}
     echo ""
 
+    # Create a ServiceAccount to let our container startup as a privileged pod
+    oc create serviceaccount siab
+    echo ""
+
+    # As `cluster-admin`, give the ServiceAccount the permission start as an anyuid pod (in our case, `UID 1001` or `developer:developer`)
+    oc adm policy add-scc-to-user anyuid -z siab
+    echo ""
+
     ### Deploy the Application
     oc new-app siab --name siab -n ${PROJECT} --allow-missing-imagestream-tags
+    echo ""
+
+    # Apply the ServiceAccount to the DeploymentConfig
+    oc patch deployment/siab --patch '{"spec":{"template":{"spec":{"serviceAccountName": "siab"}}}}'
+    echo ""
+
+    # Expose our shellinabox container
+    oc expose svc/siab
+    echo ""
+    
+    # Extend the session idle timeout to 10 minutes
+    oc annotate route siab --overwrite haproxy.router.openshift.io/timeout=600s
     echo ""
 fi
